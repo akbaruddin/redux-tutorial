1861098c4370cd682e7a55d925b7ee7bf3742052
Akbar <akbaruddin@live.in>
2021-02-09 18:22:02 +0530

    lesson 02

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 import { createStore } from "redux";
 
+// cuboid
 const initState = {
-	count: 0,
+	l: 0,
+	b: 0,
+	h: 0,
+	volume: 0,
+	area: 0,
+	total: 0
 }
 
 const reducer = (state = initState, action) => {
 	switch(action.type) {
-		case "ADD":
+		case "HIGHT_INCREASE":
 			return {
-				count: state.count + 1
+				...state,
+				h: state.h + 1,
+				volume: (state.h + 1) * state.l * state.b,
+				area: 2 * (state.h + 1) * (state.l + state.b),
+				total: 2 * ((state.l * (state.h + 1)) + (state.b * (state.h + 1)) + (state.l * state.b))
 			}
-		case "MINUS":
+		case "DEEP_INCREASE":
 			return {
-				count: state.count - 1
+				...state,
+				b: state.b + 1,
+				volume: state.h * state.l * (state.b + 1),
+				area: 2 * state.h * (state.l + (state.b + 1)),
+				total: 2 * ((state.l * state.h) + ((state.b + 1) * state.h) + (state.l * (state.b + 1)))
 			}
-		case "SQUARE":
+		case "LENGTH_INCREASE":
 			return {
-				count: state.count * state.count 
+				...state,
+				l: state.l + 1,
+				volume: state.h * (state.l + 1) * state.b,
+				area: 2 * state.h * ((state.l + 1) + state.b),
+				total: 2 * (((state.l + 1) * state.h) + (state.b * state.h) + ((state.l + 1) * state.b))
 			}
 		default:
 			return state
@@ -29,10 +47,9 @@ store.subscribe(() => {
 	console.log(store.getState());
 });
 
-store.dispatch({ type: "ADD" })
-store.dispatch({ type: "ADD" })
-store.dispatch({ type: "MINUS" })
-store.dispatch({ type: "MINUS" })
-store.dispatch({ type: "ADD" })
-store.dispatch({ type: "ADD" })
-store.dispatch({ type: "SQUARE" })
+store.dispatch({ type: 'HIGHT_INCREASE' })
+store.dispatch({ type: 'DEEP_INCREASE' })
+store.dispatch({ type: 'LENGTH_INCREASE' })
+store.dispatch({ type: 'LENGTH_INCREASE' })
+store.dispatch({ type: 'LENGTH_INCREASE' })
+store.dispatch({ type: 'HIGHT_INCREASE' })
